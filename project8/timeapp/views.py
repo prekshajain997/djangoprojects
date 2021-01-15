4875fb8e02ffe66f7c469b16105eb5d4ba4bb493
preksha <prekshajain997@gmail.com>
2021-01-15 07:28:09 +0530

    firstcommit

---
+++ project8/timeapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from datetime import datetime
+# Create your views here.
+def display_time(request):
+    server_time=datetime.now()
+    data={'time':server_time}
+
+    return render(request=request, template_name='timeapp/time.html',context=data)
